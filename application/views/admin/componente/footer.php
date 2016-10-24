872102772f9e83efaeb8c7c1687f2595fe4e9382
Peter Clayder <peterclayder@gmail.com>
2016-10-24 03:35:41 -0200

    commit inicial

---
+++ application/views/admin/componente/footer.php
@@ -0,0 +1,14 @@
+</div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="<?php echo base_url('assets/js/jquery.js');?>"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js');?>"></script>
+    <script src="<?php echo base_url('assets/admin/js/funcoes.js');?>"></script>	
+    <script src="<?php echo base_url('assets/admin/tinymce/tinymce/tinymce.min.js');?>"></script>	
+    <script src="<?php echo base_url('assets/admin/tinymce/tinymce.init.js');?>"></script>	
+    
+</body>
+
+</html>
